28cd8f75fffacf034ca6fda5481ef1c32efa26f3
Gabriel Zambuzi <gabriel.zambuzi@mblabs.com.br>
2022-03-17 21:09:44 -0300

    feat: increment login test

--- tests/integration/login.test.ts
+++ tests/integration/login.test.ts
@@ -13,4 +13,26 @@ describe('Authentication', () => {
 
     expect(response.status).toBe(200);
   });
+
+  it('should not authenticate with invalid credentials', async () => {
+    const response = await request(server)
+      .post('/api/login')
+      .send({
+        email: 'gabrielzambuzi7@gmail.com',
+        password: '123456',
+      });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should return JWT token when authenticate', async () => {
+    const response = await request(server)
+      .post('/api/login')
+      .send({
+        email: 'gabrielzambuzi7@gmail.com',
+        password: '123123',
+      });
+
+    expect(response.body).toHaveProperty('token');
+  });
 });
